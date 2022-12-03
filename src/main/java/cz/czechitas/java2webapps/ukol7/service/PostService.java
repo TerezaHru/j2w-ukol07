c22a740d429dc5a553302bc79594a9f5448ec39d
Tereza Hrušková <Ter.Hruskova@seznam.cz>
2022-12-03 14:21:53 +0100

    Doplnění tabulky

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/service/PostService.java
@@ -0,0 +1,30 @@
+package cz.czechitas.java2webapps.ukol7.service;
+
+import cz.czechitas.java2webapps.ukol7.entity.Post;
+import cz.czechitas.java2webapps.ukol7.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Služba pro práci s osobami a adresami.
+ */
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    /**
+     * Vrací stránkovaný seznam všech osob v databázi seřazených podle příjmení a jména.
+     */
+    public Page<Post> seznamPost (Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+}
+
